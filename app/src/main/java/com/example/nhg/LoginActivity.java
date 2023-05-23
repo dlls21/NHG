5fd2baccbf84087ef1addd0821467c2ef67cdcb0
dlls21 <hre1357@gmail.com>
2023-05-23 11:41:30 +0900

    로그인 화면 구현 (이메일로 로그인 버튼, 메인 컬러 적용 등)

---
+++ app/src/main/java/com/example/nhg/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.nhg;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+}
